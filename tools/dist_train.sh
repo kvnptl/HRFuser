99ca5cd023bba05572868f790aa9d41670aeba21
kvnptl <kevinpatel4400@gmail.com>
2023-10-22 01:02:28 +0200

    [Train] update multi-gpu training

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -20,5 +20,5 @@ done
 echo "PORT: $PORT is free, continue with code: "
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+python -m torch.distributed.run --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}

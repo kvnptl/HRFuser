43ed60b7343f69dbb33e03a44fadd4b0a08cc527
kvnptl <kevinpatel4400@gmail.com>
2023-11-17 23:14:42 +0100

    New modalities config files

---
+++ configs/hrfuser/cascade_rcnn_hrfuser_t_1x_stf_r1248_c_l_r_3mod_bn.py
@@ -0,0 +1,10 @@
+_base_ = [
+    './cascade_rcnn_hrfuser_t_1x_stf_r1248_c_l_r_3mod_orig_coco.py'
+]
+norm_cfg = dict(type='BN', requires_grad=True, momentum=0.1)
+model = dict(
+    type='CascadeRCNN',
+    backbone=dict(
+        norm_cfg=norm_cfg
+    )
+)
